e8023e1fc9ac39dc2e3ff214d7464ff5b7aab3b0
The-Pitstop-Studios <dsmith@trcoa.edu>
2024-09-10 13:29:40 -0500

    update

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -48,40 +48,79 @@ $(document).ready(function(){
 	}, [
 		{
 			title:"Cro Magnon Man",
-			mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
 		},
 		{
 			title:"Your Face",
-			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
 		},
 		{
 			title:"Cyber Sonnet",
-			mp3:"http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
 		},
 		{
 			title:"Tempered Song",
-			mp3:"http://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg"
 		},
 		{
 			title:"Hidden",
-			mp3:"http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
 		},
 		{
 			title:"Lentement",
 			free:true,
-			mp3:"http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/Miaow-03-Lentement.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/Miaow-03-Lentement.ogg"
 		},
 		{
 			title:"Lismore",
-			mp3:"http://www.jplayer.org/audio/mp3/Miaow-04-Lismore.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/Miaow-04-Lismore.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/Miaow-04-Lismore.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/Miaow-04-Lismore.ogg"
 		}		
+	], {
+		swfPath: "../../dist/jplayer",
+		supplied: "oga, mp3",
+		wmode: "window",
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+
+	$("#jquery_jplayer_5").jPlayer({
+		ready: function () {
+			$(this).jPlayer("setMedia", {
+			});
+		},
+		play: function() { // To avoid multiple jPlayers playing together.
+			$(this).jPlayer("pauseOthers");
+		},
+		swfPath: "../js",
+		supplied: "m4a, oga",
+		cssSelectorAncestor: "#jp_container_5",
+		wmode: "window",
+		globalVolume: true,
+		useStateClassSkin: true,
+		autoBlur: false,
+		smoothPlayBar: true,
+		keyEnabled: true
+	});
+
+	new jPlayerPlaylist({
+		jPlayer: "#jquery_jplayer_5",
+		cssSelectorAncestor: "#jp_container_5"
+	}, [
+		{
+			title:"Cro Magnon Man",
+			mp3:"https://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
+		},
+			
 	], {
 		swfPath: "../../dist/jplayer",
 		supplied: "oga, mp3",
@@ -98,39 +137,39 @@ $(document).ready(function(){
 	}, [
 		{
 			title:"Cro Magnon Man",
-			mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
 		},
 		{
 			title:"Your Face",
-			mp3:"http://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
 		},
 		{
 			title:"Cyber Sonnet",
-			mp3:"http://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
 		},
 		{
 			title:"Tempered Song",
-			mp3:"http://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg"
 		},
 		{
 			title:"Hidden",
-			mp3:"http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
 		},
 		{
 			title:"Lentement",
 			free:true,
-			mp3:"http://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/Miaow-03-Lentement.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/Miaow-03-Lentement.ogg"
 		},
 		{
 			title:"Lismore",
-			mp3:"http://www.jplayer.org/audio/mp3/Miaow-04-Lismore.mp3",
-			oga:"http://www.jplayer.org/audio/ogg/Miaow-04-Lismore.ogg"
+			mp3:"https://www.jplayer.org/audio/mp3/Miaow-04-Lismore.mp3",
+			oga:"https://www.jplayer.org/audio/ogg/Miaow-04-Lismore.ogg"
 		}		
 	], {
 		swfPath: "../../dist/jplayer",
