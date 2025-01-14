1066ddff912f4cffda40209d8195bd3edbb7ea84
The-Pitstop-Studios <dsmith@trcoa.edu>
2025-01-14 13:52:43 -0600

    hggh

--- assets/js/playlist.js
+++ assets/js/playlist.js
@@ -46,42 +46,22 @@ $(document).ready(function(){
 		jPlayer: "#jquery_jplayer_1",
 		cssSelectorAncestor: "#jp_container_1"
 	}, [
+		// Portfolio Playlist
 		{
-			title:"Cro Magnon Man",
-			mp3:"https://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
-			oga:"https://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg"
+			title:" Better Days - Plugmeindee ",
+			mp3:"assets/mp3/Better.mp3",
+
 		},
 		{
-			title:"Your Face",
-			mp3:"https://www.jplayer.org/audio/mp3/TSP-05-Your_face.mp3",
-			oga:"https://www.jplayer.org/audio/ogg/TSP-05-Your_face.ogg"
+			title:" All Night - Plugmeindee ",
+			mp3:"assets/mp3/All.mp3",
+
 		},
 		{
-			title:"Cyber Sonnet",
-			mp3:"https://www.jplayer.org/audio/mp3/TSP-07-Cybersonnet.mp3",
-			oga:"https://www.jplayer.org/audio/ogg/TSP-07-Cybersonnet.ogg"
+			title:" Impala 86 - Plugmeindee ",
+			mp3:"assets/mp3/Brick.mp3",
+
 		},
-		{
-			title:"Tempered Song",
-			mp3:"https://www.jplayer.org/audio/mp3/Miaow-01-Tempered-song.mp3",
-			oga:"https://www.jplayer.org/audio/ogg/Miaow-01-Tempered-song.ogg"
-		},
-		{
-			title:"Hidden",
-			mp3:"https://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
-			oga:"https://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg"
-		},
-		{
-			title:"Lentement",
-			free:true,
-			mp3:"https://www.jplayer.org/audio/mp3/Miaow-03-Lentement.mp3",
-			oga:"https://www.jplayer.org/audio/ogg/Miaow-03-Lentement.ogg"
-		},
-		{
-			title:"Lismore",
-			mp3:"https://www.jplayer.org/audio/mp3/Miaow-04-Lismore.mp3",
-			oga:"https://www.jplayer.org/audio/ogg/Miaow-04-Lismore.ogg"
-		}		
 	], {
 		swfPath: "../../dist/jplayer",
 		supplied: "oga, mp3",
